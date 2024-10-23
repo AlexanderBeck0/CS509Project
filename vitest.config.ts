1be9e657fd2df5e7a6dc9d6f45a869d7aecbc4e3
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-10-23 13:21:15 -0400

    Added nextjs, added some packages, and configured package.json

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        coverage: {
+            exclude: ['*.config.*', ...coverageConfigDefaults.exclude]
+        }
+    }
+})
