1de6ca9835a1d7e99a99d2213b187042d8aab65b
Emilia Krum <ejkrum@yahoo.com>
2024-10-30 10:20:04 -0400

    Separated Dropdown and SearchBar into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,113 +3,31 @@ import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import { Routes, Route, Link, HashRouter, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect, createContext, useContext, useRef } from "react";
+import Dropdown from "./components/Dropdown";
+import SearchBar from "./components/SearchBar";
 
 function AppContent() {
   async function onLogin(token: string): Promise<void> {
     //"use server"; // removed bc causing compile issues
     console.log("onLogin token provided " + token);
   }
+
   const [searchInput, setSearchInput] = useState("");
-  const [tempSearchInput, setTempSearchInput] = useState("");
-  const navigate = useNavigate();
   const [sortBy, setSortBy] = useState("startDate_ASC");
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("Start");
-  const [isAscending, setIsAscending] = useState(true);
 
-  const dropdownButtonRef = useRef<HTMLButtonElement>(null);
-  const [dropdownButtonWidth, setDropdownButtonWidth] = useState(0);
-
-  useEffect(() => {
-    if (dropdownButtonRef.current) {
-      setDropdownButtonWidth(dropdownButtonRef.current.offsetWidth);
-    }
-  }, [dropdownButtonRef]);
-
-  const handleOptionClick = (option:any) => {
-    if(selectedOption === option) {
-      setIsAscending(!isAscending);
-    } else {
-      setSelectedOption(option);
-      setIsAscending(true);
-    }
-    setIsOpen(false);
+  const handleSearch = (input: any) => {
+    setSearchInput(input);
   };
-
-  const handleInputChange = (e: any) => {
-    setTempSearchInput(e.target.value);
-  };
-
-  const handleSearch = () => {
-    setSearchInput(tempSearchInput);
-  };
-
-  useEffect(() => {
-    var sort = "";
-    switch(selectedOption) {
-      case "Name":
-        sort += "name"
-        break;
-      case "Price":
-        sort += "price";
-        break;
-      case "Start":
-        sort += "startDate";
-        break;
-      case "End":
-        sort += "endDate";
-        break;
-      default:
-        sort += "name"
-    }
-    sort += "_";
-    sort += isAscending ? "ASC" : "DESC";
-    setSortBy(sort);
-  }), [selectedOption, isAscending];
 
   return (
     <main className="main-container">
       <div className="heading">
         <Link to="/"><button className="HomeButton">Auction House</button></Link>
-        <div className="search">
-          <div className="searchBar">
-            <input
-              className="search-input input-bordered w-full max-w-xs max-h-9 input-primary focus:outline-accent"
-              type="text"
-              maxLength={20}
-              value={tempSearchInput}
-              onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
-              onChange={handleInputChange}
-              placeholder="Search here..."
-            />
-            <button onClick={handleSearch}>&nbsp;🔎&nbsp;</button> 
-          </div>  
-          <div style={{ position: "relative", marginLeft: "8px" }}>
-            <button ref={dropdownButtonRef} onClick={() => setIsOpen(!isOpen)}>
-              <span className={`icon ${isOpen ? "open" : "closed"}`}>▼</span>
-            </button>
-            {isOpen && (
-              <ul className="dropdown" style={{ right: `${dropdownButtonWidth / 2}px`, top: "120%" }}>
-                {["Name", "Price", "Start", "End"].map((option) => (
-                  <li
-                    key={option}
-                    onClick={() => handleOptionClick(option)}
-                    style={{
-                      display: "flex", alignItems: "center", cursor: "pointer",
-                      padding: "8px",
-                      backgroundColor: option === selectedOption ? "#f0f0f0" : "white", // Highlight selected option
-                      fontWeight: option === selectedOption ? "bold" : "normal",        // Bold selected option
-                    }}
-                  >
-                    {option === selectedOption && (isAscending ? "▼" : "▲")} 
-                    <span style={{ marginLeft: "4px" }}>{option}</span>
-                  </li>
-                ))}
-              </ul>
-            )} 
-          </div> 
+        <div className="search">{/* Need to disable if seller */}
+          <SearchBar handleSearch={handleSearch}/>
+          <Dropdown setSortBy={setSortBy}/> 
         </div>
-        <Link to="/Login">
+        <Link to="/Login"> {/* Need link to other pages if logged in */}
           <button className="AccountButton" style={{ height: "100%", display: "flex", alignItems: "center" }}>
             <img src="accountSymbol.png" style={{ height: "40px", width: "auto", objectFit: "contain" }} alt="Account"/>
           </button>
