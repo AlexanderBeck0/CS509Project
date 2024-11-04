f8f26665d5c570d01048db8956827a6c283a057a
BrentWeiffenbach <rweiff2022@gmail.com>
2024-11-04 13:22:47 -0500

    added view-db.html which is in s3 bucket for now to check work on db, added AdminPage if acccountType is admin and should display a page similar to view-db.html

--- src/app/components/AccountPage.tsx
+++ src/app/components/AccountPage.tsx
@@ -1,16 +1,20 @@
-import React, { useState, useEffect} from 'react';
 import SellerPage from './SellerPage';
+import AdminPage from './AdminPage';
 
 interface AccountPageProps {
     accountType: string;
 }
 
-export default function AccountPage( {accountType}:AccountPageProps) { 
-    
-    if(accountType === "seller") {
+export default function AccountPage({ accountType }: AccountPageProps) {
+
+    if (accountType === "seller") {
         return (
-            <SellerPage/>
+            <SellerPage />
         );
+    } else if (accountType === "admin") {
+        return (
+            <AdminPage />
+        )
     } else {
         return (
             <div>
