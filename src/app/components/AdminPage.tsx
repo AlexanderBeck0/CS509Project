aa05574bd04b5053865711ca8f7de44297a52475
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-11-14 18:55:33 -0500

    Added deleting rows to the Admin page

--- src/app/components/AdminPage.tsx
+++ src/app/components/AdminPage.tsx
@@ -6,11 +6,31 @@ interface AccountPageProps {
 }
 
 export default function AdminPage(props: AccountPageProps) {
+    /**
+     * The possible tables that an Admin can view.
+     * 
+     * @see {@link selectedRef}
+     */
+    const TABLES = ["Account", "Item", "Bid"] as const;
+
+    /**
+     * The ref that stores what the user currently has selected. Used a ref instead of a State so there is no rerendering.
+     * 
+     * Uses a trick of `typeof STR_ARR[number]` to determine the allowed values in a single place, {@link TABLES}, while also being able to
+     * loop through the possible values (you cannot do this with types).
+     * 
+     * @see {@link TABLES}
+     */
+    const selectedRef = useRef<null | typeof TABLES[number]>(null);
+
     const outputRef = useRef<HTMLDivElement | null>(null);
     const [outputContent, setOutputContent] = useState<ReactNode>(null);
     const [copyOnClick, setCopyOnClick] = useState<boolean>(false);
+
     // #region view-db
-    async function handleClick(tableName: string) {
+    async function handleClick(tableName: typeof TABLES[number]) {
+        selectedRef.current = tableName;
+
         const payload = {
             "tableName": tableName,
         };
@@ -31,6 +51,7 @@ export default function AdminPage(props: AccountPageProps) {
         const payload = {
             "sqlCommand": sqlCommand,
         };
+        console.log("Running SQL Command: " + sqlCommand);
         const response = await fetch("https://bgsfn1wls6.execute-api.us-east-1.amazonaws.com/initial/modify-db", {
             method: "POST",
             body: JSON.stringify(payload),
@@ -40,7 +61,7 @@ export default function AdminPage(props: AccountPageProps) {
         });
         const result = await response.json();
         if (response.ok) {
-            console.log(result.body);
+            console.log(result);
             alert("Operation successful");
             setOutputContent(<code>{result.body}</code>)
         } else {
@@ -72,6 +93,26 @@ export default function AdminPage(props: AccountPageProps) {
         props.logout();
     };
 
+    async function deleteRow(row: Record<string, string | number | boolean>): Promise<void> {
+        if (selectedRef === null) throw new Error("Can not determine what table to delete row from.");
+
+        const PRIMARY_KEYS = ['id', 'username'];
+        // Get the key name
+        const keyName = Object.keys(row).filter(key => PRIMARY_KEYS.includes(key));
+
+        if (keyName.length < 1) throw new Error("There is no known primary key in the row.");
+        if (keyName.length > 1) throw new Error("There are too many primary keys in the row.");
+        // keyName[0] is one of the PRIMARY_KEYS that relates to row
+        const key = row[keyName[0]];
+
+        await handleModify(`DELETE FROM ${selectedRef.current} WHERE ${keyName[0]} = '${key}'`);
+    }
+
+    /**
+     * Displays the data in a table.
+     * @param data The data to display the results from.
+     * @returns The ReactNode of the table from the data.
+     */
     function displayResult(data: string): ReactNode {
         const jsonData = JSON.parse(data);
         if (jsonData.length === 0) {
@@ -86,19 +127,25 @@ export default function AdminPage(props: AccountPageProps) {
                                 return <th key={index} className="p-1 text-left border-b border-b-zinc-300 whitespace-nowrap">{key}</th>
                             })
                         }
+                        <th className="p-1 test-left border-b border-b-zinc-300 whitespace-nowrap">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         jsonData.map((row: Record<string, string | number | boolean>, rowIndex: number) => {
-                            return (<tr key={rowIndex}>
-                                {
-                                    Object.values(row).map((value: string | number | boolean, index) => {
-                                        return <td key={index} className={`p-1 text-left border-b border-b-zinc-300 truncate text-wrap max-w-[150px] ${copyOnClick ? "hover:cursor-copy" : "hover:cursor-text" }`} 
-                                        title={String(value)} onClick={() => copyOnClick && copy(String(value))}>{value}</td>
-                                    })
-                                }
-                            </tr>
+                            return (
+                                <tr key={rowIndex}>
+                                    {
+                                        Object.values(row).map((value: string | number | boolean, index) => {
+                                            return <td key={index} className={`p-1 text-left border-b border-b-zinc-300 truncate text-wrap max-w-[${Math.floor(1800 / Object.values(row).length)}px] ${copyOnClick ? "hover:cursor-copy" : "hover:cursor-text"}`}
+                                                title={String(value)} onClick={() => copyOnClick && copy(String(value))}>{value}</td>
+                                        })
+                                    }
+                                    <td className={`p-1 text-left border-b border-b-zinc-300 truncate text-wrap max-w-[${Math.floor(1800 / Object.values(row).length)}px]`}>
+                                        <button onClick={() => deleteRow(row)}
+                                            className={`p-2 bg-red-400 hover:bg-red-500 active:bg-red-600`}>Delete</button>
+                                    </td>
+                                </tr>
                             )
                         })
                     }
@@ -127,16 +174,13 @@ export default function AdminPage(props: AccountPageProps) {
             <div className="viewDB"> {/* view DB content */}
                 <h1>View Database Tables: </h1>
                 <div>
-                    <button onClick={() => handleClick('Account')}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Account
-                    </button>
-                    <button onClick={() => handleClick('Item')}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Item</button>
-                    <button onClick={() => handleClick('Bid')}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Bid</button>
+                    {
+                        // Add all the tables in TABLES
+                        Object.values(TABLES).map((table, index) => {
+                            return <button key={index} onClick={() => handleClick(table)}
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{table}</button>
+                        })
+                    }
                     <button onClick={handleCopyOnClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         {copyOnClick ? "Disable" : "Enable"} Copy on Click
                     </button>
