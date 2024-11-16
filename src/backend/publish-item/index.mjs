772f467c0b0a7c3f4f434755363c729c68f36602
BrentWeiffenbach <rweiff2022@gmail.com>
2024-11-16 13:16:30 -0500

    fixed publish and unpublish items not closing pool, and some small error handling and misc problems

--- src/backend/publish-item/index.mjs
+++ src/backend/publish-item/index.mjs
@@ -1,39 +1,4 @@
-import { createPool, getUsernameFromToken } from "../opt/nodejs/index.mjs";
-
-/**
- * @param {number} id the id of item to get status of
- * @param {mysql.Pool} pool The pool to get the item from
- */
-async function getStatusFromID(id, pool) {
-    const localQuery = `
-    SELECT status FROM Item
-    WHERE id = ?
-    `;
-    return new Promise((resolve, reject) => {
-        pool.query(localQuery, [id], (err, results) => {
-            if (err) return reject(err);
-            return resolve(results[0]['status']);
-        });
-    });
-}
-
-/**
- * @param {number} id the id of item to get endDate of
- * @param {mysql.Pool} pool The pool to get the item from
- */
-async function getEndDateFromID(id, pool) {
-    const localQuery = `
-    SELECT endDate FROM Item
-    WHERE id = ?
-    `;
-    return new Promise((resolve, reject) => {
-        pool.query(localQuery, [id], (err, results) => {
-            if (err) return reject(err);
-            return resolve(results[0]['endDate']);
-        });
-    });
-}
-
+import { createPool, getUsernameFromToken, getItemFromID } from "../opt/nodejs/index.mjs";
 
 /**
  * 
@@ -51,50 +16,61 @@ export const handler = async (event) => {
     }
 
     const { token, item_id, forSale } = event;
+    try {
+        // make sure user is seller of this item
+        const username = await getUsernameFromToken(token)
+        const checkSellerQuery = 'SELECT seller_username FROM Item WHERE id = ?';
 
-    // make sure user is seller of this item
-    const username = await getUsernameFromToken(token)
-    const checkSellerQuery = 'SELECT seller_username FROM Item WHERE id = ?';
-
-    const sellerQueryResults = await new Promise((resolve, reject) => {
-        pool.query(checkSellerQuery, [item_id], (error, results) => {
-            if (error) return reject(error);
-            return resolve(results);
+        const sellerQueryResults = await new Promise((resolve, reject) => {
+            pool.query(checkSellerQuery, [item_id], (error, results) => {
+                if (error) return reject(error);
+                return resolve(results);
+            });
         });
-    });
-    console.log('Seller Query Results:', sellerQueryResults[0]);
+        // console.log('Seller Query Results:', sellerQueryResults[0]);
 
-    if (sellerQueryResults[0]['seller_username'] !== username) {
-        return { statusCode: 400, error: "You do not own this item" };
-    }
+        if (sellerQueryResults[0]['seller_username'] !== username) {
+            return { statusCode: 400, error: "You do not own this item" };
+        }
 
-    // get the status of the item
-    const status = await getStatusFromID(item_id, pool)
-    // if item status is active, can not publish
-    if (status != 'Inactive') {
-        return { statusCode: 400, error: "Cannot publish an item if it is not inactive" };
-    }
+        //get the item
+        const item = await getItemFromID(item_id, pool, username).catch((error) => {
+            return { statusCode: 403, error: "Insufficient Permission to view this item", error };
+        });
 
-    // get endDate of item
-    const endDate = await getEndDateFromID(item_id, pool)
-    // if endDate is after now then can not be published
-    console.log("endDate", endDate)
-    console.log("endDate:", new Date(endDate), "Current Date: ", new Date())
-    if (!endDate || endDate === '0000-00-00 00:00:00' || new Date(endDate) <= new Date()) {
-        return { statusCode: 400, error: "Cannot publish an item if the end date has passed or is invalid" };
-    }
+        // if item status is active, can not publish
+        if (item.status != 'Inactive') {
+            return { statusCode: 400, error: "Cannot publish an item if it is not inactive" };
+        }
 
-    return new Promise((resolve, reject) => {
+        // if endDate is after now then can not be published
+        // console.log("endDate", item.endDate)
+        // console.log("endDate:", new Date(item.endDate), "Current Date: ", new Date())
+        if (!item.endDate || item.endDate === '0000-00-00 00:00:00' || new Date(item.endDate) <= new Date()) {
+            return { statusCode: 400, error: "Cannot publish an item if the end date has passed or is invalid" };
+        }
 
-        const localQuery = `
+        return await new Promise((resolve, reject) => {
+
+            const localQuery = `
             UPDATE Item
             SET status = 'Active', forSale = ?, startDate = NOW()
             WHERE id = ?;
             `;
 
-        pool.query(localQuery, [forSale, item_id], (err) => {
-            if (err) return reject(err);
-            return resolve({ statusCode: 200, item_id });
+            pool.query(localQuery, [forSale, item_id], (err) => {
+                if (err) return reject(err);
+                return resolve({ statusCode: 200, item_id });
+            });
         });
-    });
+    } catch (error) {
+        console.error(error);
+        return reject({
+            statusCode: 500,
+            error: error.message
+        });
+    } finally {
+        // Close the pool's connection
+        pool.end();
+    }
 }
--- src/backend/unpublish-item/index.mjs
+++ src/backend/unpublish-item/index.mjs
@@ -16,57 +16,68 @@ export const handler = async (event) => {
     }
 
     const { token, item_id } = event;
+    try {
+        // make sure user is seller of this item
+        const username = await getUsernameFromToken(token)
+        const checkSellerQuery = 'SELECT seller_username FROM Item WHERE id = ?';
 
-    // make sure user is seller of this item
-    const username = await getUsernameFromToken(token)
-    const checkSellerQuery = 'SELECT seller_username FROM Item WHERE id = ?';
-
-    const sellerQueryResults = await new Promise((resolve, reject) => {
-        pool.query(checkSellerQuery, [item_id], (error, results) => {
-            if (error) return reject(error);
-            return resolve(results);
+        const sellerQueryResults = await new Promise((resolve, reject) => {
+            pool.query(checkSellerQuery, [item_id], (error, results) => {
+                if (error) return reject(error);
+                return resolve(results);
+            });
         });
-    });
-    // console.log('Seller Query Results:', sellerQueryResults[0]);
+        // console.log('Seller Query Results:', sellerQueryResults[0]);
 
-    if (sellerQueryResults[0]['seller_username'] !== username) {
-        return { statusCode: 400, error: "You do not own this item" };
-    }
+        if (sellerQueryResults[0]['seller_username'] !== username) {
+            return { statusCode: 400, error: "You do not own this item" };
+        }
 
-    //get the item
-    const item = await getItemFromID(item_id, pool, username)
-
-    // if item status is not Active can not unpublish
-    if (item.status != 'Active') {
-        return { statusCode: 400, error: "Cannot publish an item if it is Active" };
-    }
-
-    // can not unpublish items with bids
-    const bidQuery = 'SELECT * FROM Bid WHERE item_id = ?';
-
-    const bidResults = await new Promise((resolve, reject) => {
-        pool.query(bidQuery, [item_id], (error, results) => {
-            if (error) return reject(error);
-            return resolve(results);
+        //get the item
+        const item = await getItemFromID(item_id, pool, username).catch((error) => {
+            return { statusCode: 403, error: "Insufficient Permission to view this item", error };
         });
-    });
 
-    if (bidResults.length > 0) {
-        return { statusCode: 400, error: "Cannot unpublish an item with bids" };
-    }
+        // if item status is not Active can not unpublish
+        if (item.status !== 'Active') {
+            return { statusCode: 400, error: "Cannot publish an item if it is Active" };
+        }
 
-    // proceed to unpublish
-    return new Promise((resolve, reject) => {
+        // can not unpublish items with bids
+        const bidQuery = 'SELECT * FROM Bid WHERE item_id = ?';
 
-        const localQuery = `
+        const bidResults = await new Promise((resolve, reject) => {
+            pool.query(bidQuery, [item_id], (error, results) => {
+                if (error) return reject(error);
+                return resolve(results);
+            });
+        });
+
+        if (bidResults.length > 0) {
+            return { statusCode: 400, error: "Cannot unpublish an item with bids" };
+        }
+
+        // proceed to unpublish
+        return await new Promise((resolve, reject) => {
+            const localQuery = `
             UPDATE Item
             SET status = 'Inactive', forSale = 0
             WHERE id = ?;
             `;
 
-        pool.query(localQuery, [item_id], (err) => {
-            if (err) return reject(err);
-            return resolve({ statusCode: 200, item_id });
+            pool.query(localQuery, [item_id], (err) => {
+                if (err) return reject(err);
+                return resolve({ statusCode: 200, item_id });
+            });
         });
-    });
+    } catch (error) {
+        console.error(error);
+        return reject({
+            statusCode: 500,
+            error: error.message
+        });
+    } finally {
+        // Close the pool's connection
+        pool.end();
+    }
 }
