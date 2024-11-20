ad9a453122152ad135e9c8ab9cbe45ee6b17c44a
Emmy Krum <ejkrum@yahoo.com>
2024-11-19 20:54:39 -0500

    search item lambda updated to include recently sold

--- src/backend/search-item/index.mjs
+++ src/backend/search-item/index.mjs
@@ -9,7 +9,7 @@ export const handler = async (event)  => {
       console.error("Failed to create MySQL Pool. Error: " + JSON.stringify(error));
       return { statusCode: 500, error: "Could not make database connection" };
   }
-  let SearchDB = (query, sortBy) => {
+  let SearchDB = (query, sortBy, recentlySold) => {
     console.log("Searching...");
     let [sort, order] = sortBy.split('_');
     
@@ -20,16 +20,67 @@ export const handler = async (event)  => {
     console.log("Sort by: "+sort);
     console.log("Order: "+order);
 
+    const isEmptyQuery = !query || query.trim() === "";
+    let isNumeric = isEmptyQuery ? false : !isNaN(query);
+    let isDate = /^\d{2}\/\d{2}\/\d{4}$/.test(query); 
+    let formattedDate = null;
+
+    if (isDate) {
+      try {
+        const [month, day, year] = query.split('/');
+        if (!isNaN(new Date(`${year}-${month}-${day}`).getTime())) {
+          formattedDate = `${year}-${month}-${day}`;
+        }
+      } catch (err) {
+        console.error("Invalid date format: " + query);
+      }
+    }
+
+    console.log(formattedDate);
+
     query = `%${query}%`;
     return new Promise((resolve, reject) => {
-      const sqlQuery = `
-          SELECT * FROM Item 
-          WHERE status = 'Active' 
-          AND (name LIKE ? OR description LIKE ? OR endDate LIKE ? OR price LIKE ? OR startDate LIKE ?) 
-          ORDER BY ${sort} ${order}, startDate, endDate, price, name
+      let sqlQuery = `
+      SELECT * FROM Item
       `;
+      if(recentlySold) {
+        sqlQuery += `WHERE (status = 'Completed' OR status = 'Fulfilled')
+        AND (DATE(endDate) = CURDATE() - INTERVAL 1 DAY OR DATE(endDate) = CURDATE() - INTERVAL 2 DAY)
+        `;
+      } else {
+        sqlQuery += `WHERE status = 'Active'
+        `;
+      }
+      
+      const params = [];
+
+      if (!isEmptyQuery) {
+        sqlQuery += `
+          AND (
+            name LIKE ? OR 
+            description LIKE ?
+        `;
+        params.push(query, query);
+  
+        if (isNumeric) {
+          sqlQuery += ` OR price LIKE ?
+          `;
+          params.push(query);
+        }
+  
+        if (formattedDate) {
+          sqlQuery += ` OR DATE(startDate) = ? OR DATE(endDate) = ?
+          `;
+          params.push(formattedDate, formattedDate);
+        }
+  
+        sqlQuery += `)
+        `;
+      }
+      sqlQuery += `ORDER BY ${sort} ${order}, startDate, endDate, price, name`;
+
       console.log("Query: "+sqlQuery);
-      pool.query(sqlQuery, [query, query, query, query, query], (error, rows) => {
+      pool.query(sqlQuery, params, (error, rows) => {
         if (error) { console.log("DB error"); return reject(error); }
         return resolve(rows);
       });
@@ -39,7 +90,7 @@ export const handler = async (event)  => {
   let response = undefined;
 
   try {
-    let results = await SearchDB(event.query, event.sortBy);
+    let results  = await SearchDB(event.query, event.sortBy, event.recentlySold);
     console.log(results);
     const filteredResults = results.map(({ id, image, name, description, price, startDate, endDate }) => ({
         id, image, name, description, price, startDate, endDate
