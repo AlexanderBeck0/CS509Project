b9c149bf9dc9ee015ef83b892148227479fbd423
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-12-10 16:43:31 -0500

    Fixed Same price bid allowed #63

--- src/app/components/ItemPage/BuyerItemPage.tsx
+++ src/app/components/ItemPage/BuyerItemPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 interface BuyerItemPageProps {
     status: string;
@@ -9,14 +9,14 @@ interface BuyerItemPageProps {
 }
 
 export default function BuyerItemPage(props: BuyerItemPageProps) {
-    const [newBid, setNewBid] = useState<number>(props.price);
+    const bidRef = useRef<HTMLInputElement | null>(null);
     // get funds from getToken
     const handlePlaceBid = () => {
         const placeBid = async () => {
             const payload = {
                 token: localStorage.getItem('token'),
                 id: props.item_id,
-                bid: newBid,
+                bid: bidRef.current!.valueAsNumber,
             };
 
             await fetch("https://bgsfn1wls6.execute-api.us-east-1.amazonaws.com/initial/placeBid", {
@@ -37,14 +37,13 @@ export default function BuyerItemPage(props: BuyerItemPageProps) {
                 // Log actual errors and not just insufficient permission errors
                 if (error instanceof Error) console.error(error);
                 if (typeof error === 'string' && error.includes("Insufficient funds")) {
-                    alert(`Insufficient funds to ${props.itemForSale ? "purchase": "bid on"} this item`);
+                    alert(`Insufficient funds to ${props.itemForSale ? "purchase" : "bid on"} this item`);
                 }
                 if (typeof error === 'string') alert(error)
             });
         };
 
         placeBid();
-        // setReload(reload+1);
     };
 
     if (props.status === "Active") {
@@ -55,10 +54,10 @@ export default function BuyerItemPage(props: BuyerItemPageProps) {
                         Place bid of: $
                         <input
                             type="number"
-                            value={newBid}
-                            onChange={(e) => setNewBid(Number(e.target.value))}
+                            ref={bidRef}
+                            defaultValue={props.price + 1}
                             style={{ marginLeft: '0.5rem', padding: '0.25rem' }}
-                            min={props.price}
+                            min={props.price + 1}
                             step={1}
                         />
                     </label>
